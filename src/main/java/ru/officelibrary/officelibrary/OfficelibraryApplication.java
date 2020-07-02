e581a0ddda9e0a5de4c8b4d47ab047023e279e81
Алексей Корж <alexey.korzh.kas@gmail.com>
2020-07-02 09:36:12 +0600

    Initial commit

---
+++ src/main/java/ru/officelibrary/officelibrary/OfficelibraryApplication.java
@@ -0,0 +1,13 @@
+package ru.officelibrary.officelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficelibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficelibraryApplication.class, args);
+    }
+
+}
